b306c07cc6d4a6365b1b924f4db0cc84eefacb0b
krozz <krozzgaming9@gmail.com>
2026-01-19 21:29:24 +0100

    Currency bug fix

--- src/data/currencies.js
+++ src/data/currencies.js
@@ -1,5 +1,5 @@
 console.log("Selecton: Loading currencies.js");
-export const currenciesList = {
+const currenciesList = {
     AUD: { name: "Australian Dollar", symbol: "A$", rate: 1.29009, keywords: ["australian dollar", "австралийских доллар"] },
     BGN: { name: "Bulgarian Lev", symbol: "лв", rate: 1.640562 },
     BRL: { name: "Brazilian real", symbol: "R$", rate: 5.616101 },
